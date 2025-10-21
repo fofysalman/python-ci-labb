36b21380d06a1c06c34797ddf62a150ea0d4ffc4
fofysalman <fatima.mohamed.9592@gmail.com>
2025-10-21 15:28:57 +0200

    Add divide() and tests

---
+++ test_calculator.py
@@ -0,0 +1,23 @@
+from calculator import add, subtract, multiply
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(10, 3) == 7
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+
+import pytest
+from calculator import divide
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raise8ValueError9:
+        divide(10,0)
+
+        
